35ca59c652c79438c53bf449a08b5bb733d949fb
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2023-03-31 09:36:26 -0600

    attack button

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+const getRandomValue = (min,max) => Math.floor(Math.random()*(max-min))+min
+
 const app = Vue.createApp({
     data(){
         return({
-            monsterHealthValue: 100,
-            yourHealthValue: 100,
+            monsterHealth: 100,
+            playerHealth: 100,
         })
     },
     methods: {
-        attack(){
-
+        attackMonster(){
+            this.monsterHealth -= getRandomValue(5,12)
+            this.attackPlayer()
+        },
+        attackPlayer(){
+            this.playerHealth -= getRandomValue(8,15)
         },
         specialAttack(){
-
+            this.monsterHealth -= getRandomValue(50,25)
         },
         heal(){
-
+            this.playerHealth += getRandomValue(25,10)
         },
         surrender(){
-
+            this.playerHealth = 0
         }
     }
 })
