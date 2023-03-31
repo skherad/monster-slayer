8300e94323455d6d20f95c34e9f26eac536c06aa
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2023-03-31 09:10:32 -0600

    git init

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data(){
+        return({
+            monsterHealthValue: 100,
+            yourHealthValue: 100,
+        })
+    },
+    methods: {
+        attack(){
+
+        },
+        specialAttack(){
+
+        },
+        heal(){
+
+        },
+        surrender(){
+
+        }
+    }
+})
+
+app.mount('#game')
